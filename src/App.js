4efa865d2d06167972fb749e8dcd8fe66a408a10
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-10-16 21:55:18 +0300

    create form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Form from "./components/Form";
+
 function App() {
   return (
     <>
       <h1>To Do App</h1>
+      <Form />
     </>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { FaPlus } from "react-icons/fa";
+
+const Form = () => {
+  return (
+    <>
+      <div className="form">
+        <form autoComplete="off">
+          <div className="input-and-button">
+            <input type="text" placeholder="Add to do items" required />
+            <div className="button">
+              <button type="submit">
+                <FaPlus />
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
