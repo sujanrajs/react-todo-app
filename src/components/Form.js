a3238784e63a16ae831a6cea6ebaa3090c7a1ffa
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-10-16 22:26:48 +0300

    set and get item to and from local storage

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { FaPlus } from "react-icons/fa";
 
+const getTodosFromLS = () => {
+  const data = localStorage.getItem("Todos");
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
 const Form = () => {
+  const [todoValue, setTodoValue] = useState("");
+  const [todos, setTodos] = useState(getTodosFromLS());
+
+  console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const time = new Date().getTime();
+
+    let todoObject = {
+      ID: time,
+      TodoValue: todoValue,
+      completed: false,
+    };
+
+    setTodos([...todos, todoObject]);
+
+    setTodoValue("");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("Todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <>
       <div className="form">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={handleSubmit}>
           <div className="input-and-button">
-            <input type="text" placeholder="Add to do items" required />
+            <input
+              type="text"
+              required
+              placeholder="Add to do items"
+              onChange={(e) => setTodoValue(e.target.value)}
+              value={todoValue}
+            />
             <div className="button">
               <button type="submit">
-                <FaPlus size={25}/>
+                <FaPlus size={25} />
               </button>
             </div>
           </div>
